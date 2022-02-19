b6d55996ae3e902bd252fc3019fa5deaa7210060
yvs01 <singhyashvardhan0111@gmail.com>
2022-02-19 23:00:57 +0530

    Passport Facebook Oauth

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://127.0.0.1:27017/conFusion'
+    'mongoUrl': 'mongodb://127.0.0.1:27017/conFusion',
+    'facebook': {
+        clientId: '505859561057907',
+        clientSecret: 'b91dee3e0d35cacf1576f95333fbf2ec'
+    }
 }
